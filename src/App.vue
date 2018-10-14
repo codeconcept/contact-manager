80c5b2cf1a1a78be58ca1e5ef87c67c07229bbe6
codeconcept <contact@codeconcept.fr>
2018-10-14 18:23:36 +0200

    retrieving contacts from Firestore and displaying them

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <contact-form></contact-form>
+    <contact-list></contact-list>
   </div>
 </template>
 
 <script>
 import ContactForm from './components/ContactForm.vue';
+import ContactList from './components/ContactList.vue';
 
 export default {
   name: 'app',
   components: {
     ContactForm,
+    ContactList,
   },
 };
 </script>
---
+++ src/components/ContactList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="contact in contacts" :key="contact.data().id">
+        {{contact.data().firstName}} {{contact.data().lastName}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import db from '../shared/db';
+
+export default {
+  data() {
+    return {
+      contacts: [],
+    };
+  },
+  created() {
+    this.getAllContacts();
+  },
+  methods: {
+    getAllContacts() {
+      db.read().then((snapshot) => {
+        console.log(snapshot);
+        this.contacts = snapshot.docs;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
