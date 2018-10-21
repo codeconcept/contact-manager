2fb7d3c4a68d2a50e5bc21901e7bc4f6c6732500
codeconcept <contact@codeconcept.fr>
2018-10-21 11:51:48 +0200

    using router-link to client side route

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,15 @@
 <template>
-  <div id="app">
-    <contact-form @created="created"></contact-form>
-    <br>
-    <contact-list
-      :contacts="contacts"
-      @deleteContact="deleteContact"
-      @saveEdit="saveEdit">
-    </contact-list>
+  <div>
+    <router-link to="/">Accueil</router-link> |
+    <router-link to="/contact">Contact</router-link>
+    <br><br>
     <router-view/>
   </div>
 </template>
 
 <script>
-import ContactForm from './components/ContactForm.vue';
-import ContactList from './components/ContactList.vue';
-import db from './shared/db';
-
-export default {
-  name: 'app',
-  components: {
-    ContactForm,
-    ContactList,
-  },
-  data() {
-    return {
-      contacts: [],
-    };
-  },
-  created() {
-    this.getAllContacts();
-  },
-  methods: {
-    created(id) {
-      console.log(id);
-      this.getAllContacts();
-    },
-    getAllContacts() {
-      db.read().then((snapshot) => {
-        this.contacts = snapshot.docs;
-      });
-    },
-    deleteContact(contact) {
-      db.delete(contact.id).then(() => {
-        this.getAllContacts();
-      });
-    },
-    saveEdit(editedContact) {
-      db.update(editedContact).then(() => {
-        this.getAllContacts();
-      });
-    },
-  },
-  watch: {
-    contacts(newContacts) {
-      console.log('newContacts', newContacts);
-      this.contacts = newContacts;
-    },
-  },
-};
+export default {};
 </script>
 
-<style>
+<style scoped>
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,64 @@
 <template>
-  <div>
-    <h2>Accueil</h2>
+  <div id="app">
+    <contact-form @created="created"></contact-form>
+    <br>
+    <contact-list
+      :contacts="contacts"
+      @deleteContact="deleteContact"
+      @saveEdit="saveEdit">
+    </contact-list>
+    <router-view/>
   </div>
 </template>
 
 <script>
-export default {};
+import ContactForm from '../components/ContactForm.vue';
+import ContactList from '../components/ContactList.vue';
+import db from '../shared/db';
+
+export default {
+  name: 'app',
+  components: {
+    ContactForm,
+    ContactList,
+  },
+  data() {
+    return {
+      contacts: [],
+    };
+  },
+  created() {
+    this.getAllContacts();
+  },
+  methods: {
+    created(id) {
+      console.log(id);
+      this.getAllContacts();
+    },
+    getAllContacts() {
+      db.read().then((snapshot) => {
+        this.contacts = snapshot.docs;
+      });
+    },
+    deleteContact(contact) {
+      db.delete(contact.id).then(() => {
+        this.getAllContacts();
+      });
+    },
+    saveEdit(editedContact) {
+      db.update(editedContact).then(() => {
+        this.getAllContacts();
+      });
+    },
+  },
+  watch: {
+    contacts(newContacts) {
+      console.log('newContacts', newContacts);
+      this.contacts = newContacts;
+    },
+  },
+};
 </script>
 
-<style scoped>
+<style>
 </style>
